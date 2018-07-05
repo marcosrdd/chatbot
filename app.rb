fb91716f8974c9e37d9c5c67ff6707d7e4966bd1
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-07-05 17:50:38 -0300

    testing heroku 3

--- app.rb
+++ app.rb
@@ -14,13 +14,14 @@ class App < Sinatra::Base
 
   post '/webhook' do
     result = JSON.parse(request.body.read)["result"]
-    if result["contexts"].present?
-      response = "Present"
+    response = result
+    #if result["contexts"].present?
+    #  response = "Present"
       #response = InterpretService.call(result["action"], result["contexts"][0]["parameters"])
-    else
-      response = "Not present"
+    #else
+    #  response = "Not present"
       #response = InterpretService.call(result["action"], result["parameters"])
-    end
+    #end
 
     content_type :json
     {
