52ac51c975dfdd58d32f21b30fbfd0e964541374
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-07-13 10:25:11 -0300

    added params logs

--- app/services/faq/create_service.rb
+++ app/services/faq/create_service.rb
@@ -1,23 +1,24 @@
 module FaqModule
-    class CreateService
-      def initialize(params)
-        # TODO: identify origin and set company
-        @company = Company.last
-        @question = params["question-original"]
-        @answer = params["answer-original"]
-        @hashtags = params["hashtags-original"]
-      end
-  
-      def call
-        return 'Hashtag Obrigatória' if @hashtags == nil
-        Faq.transaction do
-          faq = Faq.create(question: @question, answer: @answer, company: @company)
-          return "Hashtag Obrigatória" if @hashtags == nil
-          @hashtags.split(/[\s,]+/).each do |hashtag|
-            faq.hashtags << Hashtag.create(name: hashtag)
-          end
+  class CreateService
+    def initialize(params)
+      # TODO: identify origin and set company
+      @company = Company.last
+      @question = params["question-original"]
+      @answer = params["answer-original"]
+      @hashtags = params["hashtags-original"]
+    end
+
+    def call
+      return @company.id.to_s + "question:" + @question + "answer:" + @answer + "hashtags:" + @hashtags
+      return 'Hashtag Obrigatória' if @hashtags == nil
+      Faq.transaction do
+        faq = Faq.create(question: @question, answer: @answer, company: @company)
+        return "Hashtag Obrigatória" if @hashtags == nil
+        @hashtags.split(/[\s,]+/).each do |hashtag|
+          faq.hashtags << Hashtag.create(name: hashtag)
         end
-        "Criado com sucesso. Total de perguntas #{Faq.count}"
       end
+      "Criado com sucesso. Total de perguntas #{Faq.count}"
     end
   end
+end
